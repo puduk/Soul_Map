fb3b763c7e65c175b06e36e573e90e7b4b27c575
puduk <demirmert986@gmail.com>
2025-04-20 06:16:21 +0200

    feat: add entry point (main.cpp)

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include "Soul/soul_data.hpp"
+#include "Soul/menu.hpp"
+#include <chrono>
+#include <thread>
+#include "Soul/terminal.hpp"
+#include "Soul/brain_data.hpp"
+
+void opening(){
+
+    std::string title = "Welcome to the Soul Map";
+    for (char c : title) {
+        std::cout << "\033[1m\033[38;5;183m" << c << reset_color << std::flush;
+        std::this_thread::sleep_for(std::chrono::milliseconds(80));
+    }
+    std::cout << std::endl;
+
+
+}
+
+
+int main() {
+
+    opening();
+
+
+    BrainData b;
+    SoulData s;
+    menu(s,b);
+
+
+
+
+}
+
+
