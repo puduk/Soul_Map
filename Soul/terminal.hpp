584d8967d656b83076c7f65d54bd4a74e95a654d
puduk <demirmert986@gmail.com>
2025-04-20 06:16:26 +0200

    feat: implement terminal styling header

---
+++ Soul/terminal.hpp
@@ -0,0 +1,46 @@
+#ifndef TERMINAL_H
+#define TERMINAL_H
+
+#define reset_color       "\033[0m"
+
+#define bold        "\033[1m"
+#define cool        "\033[3m"
+#define underline   "\033[4m"
+
+// questions
+#define red        "\033[38;5;168m"
+
+// user answers
+#define blue     "\033[38;5;111m"
+
+
+// answers
+#define purple    "\033[38;5;103m"
+
+
+// hafif pastel gri gibi
+#define gray     "\033[38;5;102m"
+
+
+//
+#define white   "\033[38;5;252m"
+
+// BACKGROUNDS:
+
+#define background_line  "\033[48;5;235m"  //bitis arkaplan
+#define background_main  "\033[48;5;236m"  //ana arka plan
+
+
+
+
+
+#define leader        "\033[38;5;208m"  // Sunset Orange
+#define wolf      "\033[38;5;67m"   // Steel Blue
+#define helpfull       "\033[38;5;114m"  // Soft Green
+#define adventurer    "\033[38;5;220m"  // Golden Yellow
+#define artist        "\033[38;5;183m"  // Lavender
+#define logician      "\033[38;5;123m"  // Ice Cyan
+
+
+
+#endif
